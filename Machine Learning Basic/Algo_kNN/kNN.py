bcd51971282ddf23baa7cc7fd857cf412f822918
brans-t <zhzgame11122@163.com>
2025-05-14 17:29:34 +0800

    添加分类器预测函数并优化文档字符串

--- Machine Learning Basic/Algo_kNN/kNN.py
+++ Machine Learning Basic/Algo_kNN/kNN.py
@@ -1,33 +1,38 @@
 from numpy import *
 import operator
 
-def createDataSet():
-    """样本数据"""
-    group = array([[1.0,1.1],[0,0],[1.0,1.0],[0,0.1]])
-    labels = ['A','B','A','B']
-    return group, labels
+# def createDataSet():
+#     """样本数据"""
+#     group = array([[1.0,1.1],[0,0],[1.0,1.0],[0,0.1]])
+#     labels = ['A','B','A','B']
+#     return group, labels
 
 def classify0(inX, dataSet, labels, k):
     """kNN algorithm:
-    基于训练数据集dataSet和对应的标签labels，对输入的数据集inX进行分类，k为考虑的最近邻
-    的个数。
+    基于训练数据集dataSet和对应的标签labels，对输入的数据集inX进行分类，k为考虑的最近邻的个数。
+    inX: 输入数据集
+    dataSet: 训练数据集
+    labels: 训练数据集对应的标签
+    k: 选择的最近邻的个数
     """
-    dataSetSize = dataSet.shape[0]                                #确认训练数据集中的样本数量；shape：这是 NumPy 数组的一个属性，返回一个元组，表示数组的维度
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet          #将输入向量inX复制成与训练数据集相同大小的矩阵，然后再与样本数据相减
+    dataSetSize = dataSet.shape[0]                                     #确认训练数据集中的样本数量；shape：这是 NumPy 数组的一个属性，返回一个元组，表示数组的维度
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet                    #将输入向量inX复制成与训练数据集相同大小的矩阵，然后再与样本数据相减
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)                           #计算欧氏距离；axis=1：沿着行的方向进行操作，即对每一行进行操作
+    sqDistances = sqDiffMat.sum(axis=1)                                #计算欧氏距离；axis=1：沿着行的方向进行操作，即对每一行进行操作
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()                      #对距离进行排序以，并返回数组元素的排序索引
+    sortedDistIndicies = distances.argsort()                           #对距离进行排序以，并返回数组元素的排序索引
     classCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]                   #收集前k个最近邻样本标签
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1   #统计前k个最近邻样本，每个标签出现的次数
-    sortedClassCount = sorted(classCount.items(),                    #对标签次数进行降序排列
+        voteIlabel = labels[sortedDistIndicies[i]]                     #收集前k个最近邻样本标签
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1     #统计前k个最近邻样本，每个标签出现的次数
+    sortedClassCount = sorted(classCount.items(),                      #对标签次数进行降序排列
                             key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
 def file2matrix(filename):
-    """将文本记录转换为NumPy的解析程序"""
+    """将文本记录转换为NumPy的解析程序
+    filename: 文件名
+    """
     fr = open(filename)                                         #打开文件并赋值给变量fr
     arrayOLines = fr.readlines()                                #从文件中读取所有行并存储到一个列表中
     numberOfLines = len(arrayOLines)
@@ -43,7 +48,9 @@ def file2matrix(filename):
     return returnMat, classLabelVector
 
 def autoNorm(dataSet):
-    """归一化特征值"""
+    """归一化特征值
+    dataSet: 数据集
+    """
     minVals = dataSet.min(0)                                #计算 NumPy 数组 dataSet 的每一列的最小值和最大值;参数 0 表示沿着数组的第 0 轴（即列方向）计算
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
@@ -66,3 +73,25 @@ def datingClassTest():
         print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0
     print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+
+def classifyPerson():
+    """分类器预测函数"""
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    try:
+        percentTats = float(input("percentage of time spent playing video games?"))
+        print("You spend", percentTats, "% of your time playing video games.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    try:
+        ffMiles = float(input("frequent flier miles earned per year?"))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    try:
+        iceCream = float(input("liters of ice cream consumed per year?"))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    datingDataMat, datingLabels = file2matrix('Database\datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])                                     #将用户输入的数据转换为NumPy数组
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: ", resultList[classifierResult - 1])
