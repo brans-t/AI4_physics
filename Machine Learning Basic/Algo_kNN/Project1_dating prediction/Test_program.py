b0c37b8c230c1df2cd22f7d1aeac3784e435c302
brans-t <zhzgame11122@163.com>
2025-05-14 20:05:00 +0800

    重构数据处理模块，替换kNN为date_pre，优化数据读取和归一化功能

--- Machine Learning Basic/Algo_kNN/Project1_dating prediction/Test_program.py
+++ Machine Learning Basic/Algo_kNN/Project1_dating prediction/Test_program.py
@@ -1,11 +1,14 @@
-import kNN
+import date_pre as datepre
 from numpy import *
 import matplotlib
 import matplotlib.pyplot as plt
+import os
 
+pathroot = os.path.dirname(os.path.abspath(__file__))  #获取当前文件路径
+path = os.path.join(pathroot, 'Database\datingTestSet.txt')
 
-datingDataMat, datingLabels = kNN.file2matrix('Database\datingTestSet.txt')  #读取数据集
-normMat, ranges, minVals = kNN.autoNorm(datingDataMat)  #归一化特征值
+datingDataMat, datingLabels = datepre.file2matrix(path)  #读取数据集
+normMat, ranges, minVals = datepre.autoNorm(datingDataMat)  #归一化特征值
 
 print(normMat)
 # 绘制散点图
